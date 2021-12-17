688b57d4ee618e580023e1456edb57eb04308e2b
HWH88340 <hwh88340@gmail.com>
2021-12-17 13:50:34 +0800

    first commit

---
+++ lab/lab01/lab01_extra.py
@@ -0,0 +1,60 @@
+"""Optional questions for Lab 1"""
+
+# While Loops
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if k == 0:
+        return 1
+
+    i = n - k + 1
+    product = 1
+    while i <= n:
+        product = product * i
+        i = i + 1
+    
+    return product
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    adj = 0
+    digit = 0
+    while(n > 0):
+        digit = n % 10
+        if digit == 8:
+            adj = adj + 1
+        elif digit != 8 and adj > 0:
+            adj = adj - 1
+        
+        n = n // 10
+        if adj == 2:
+            return True
+    
+    return False
+        
